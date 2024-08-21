c12182903066f8ce4514e7ea5d13b0ea0b772b08
Christoffer Hald Christensen <chrishald@duck.com>
2024-08-21 23:55:06 +0200

    Removed redundant test and added test for file loading

---
+++ tests/loading_files/test.cpp
@@ -0,0 +1,41 @@
+// Project Headers
+#include <orb/fileparsing/file_loading.hpp>
+#include <orb/log/assert.hpp>
+
+// STDLIB
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    ASSERT_EQ(argc, 3, "Must give both filename and expected output");
+    using namespace orb::fileparsing;
+    InitDefaultUnicode();
+
+    const char* filePath = argv[1];
+
+    auto result = LoadFile(filePath);
+
+    if (!result.has_value())
+    {
+        ASSERT(result.has_value(), result.error());
+    }
+
+    icu::UnicodeString expectedValue(argv[2]);
+    const icu::UnicodeString value = result.value();
+
+    if (expectedValue != value)
+    {
+        logging::Error("The value from `LoadFile` did not match the expected result");
+
+        std::string valueStr;
+        value.toUTF8String(valueStr);
+        std::string expectedStr;
+        expectedValue.toUTF8String(expectedStr);
+
+        logging::Info("Gotten value: `" + valueStr + '`');
+        logging::Info("Expected value: `" + expectedStr + '`');
+
+        // Fail the test
+        exit(1);
+    }
+}
