dcec2e3fa6258015adf610731c9331c7a1c86457
AtleSkaanes <atle.skaanes@gmail.com>
2025-08-21 19:12:59 +0200

    Added parsing files

--- src/parsing.zig
+++ src/parsing.zig
@@ -1,1 +1,4 @@
-pub const Lexer = @import("./parsing/Lexer.zig");
+pub const Lexer = @import("parsing/Lexer.zig");
+pub const Parser = @import("parsing/Parser.zig");
+pub const Token = @import("parsing/token.zig");
+pub const AstNode = @import("parsing/AstNode.zig");
