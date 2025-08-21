336b30c2fc2bd781dc0207b38a75622b5cd6ff70
Christoffer Hald Christensen <chrishald@proton.me>
2025-08-21 18:33:44 +0200

    Fixed description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "A magical programming language for bored wizards";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
