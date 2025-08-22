70fe5401df06fc5a11380cce2ac6b5822c61c172
Christoffer Hald Christensen <chrishald@proton.me>
2025-08-22 23:57:26 +0200

    Testing in the parsing.zig file and a testing directory

--- src/parsing.zig
+++ src/parsing.zig
@@ -3,3 +3,7 @@ pub const parse = @import("parsing/parser.zig").parse;
 pub const Token = @import("parsing/Lexer.zig").Token;
 pub const AstNode = @import("parsing/AstNode.zig");
 pub const AstRoot = AstNode.AstRoot;
+
+test {
+    @import("std").testing.refAllDecls(@This());
+}
