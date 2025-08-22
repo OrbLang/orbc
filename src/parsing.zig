0518fc6afaef25eb9adffd3e8302f13782529181
AtleSkaanes <atle.skaanes@gmail.com>
2025-08-22 14:56:15 +0200

    Updated parsing imports

--- src/parsing.zig
+++ src/parsing.zig
@@ -1,5 +1,5 @@
 pub const Lexer = @import("parsing/Lexer.zig");
-pub const parse = @import("parsing/parser.zig.zig").parse;
-pub const Token = @import("parsing/token.zig");
+pub const parse = @import("parsing/parser.zig").parse;
+pub const Token = @import("parsing/Lexer.zig").Token;
 pub const AstNode = @import("parsing/AstNode.zig");
 pub const AstRoot = AstNode.AstRoot;
